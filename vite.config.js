8ca8676e56747a91155fc352e9dc094b89a8d95c
Armin Amiri <78607877+zNiGhTFuRyZNTT@users.noreply.github.com>
2023-10-02 00:36:32 +1100

    some bug fixes

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    define: {
-    "global": {},
-    },
   base: '/',
   plugins: [react()],
   test: {
