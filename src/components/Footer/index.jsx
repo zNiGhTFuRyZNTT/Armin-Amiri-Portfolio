81f5cb4e89fd6e1625cf3a5fd51862228fe2ccc8
Armin Amiri <78607877+zNiGhTFuRyZNTT@users.noreply.github.com>
2023-12-23 01:00:25 +1100

    Update index.jsx

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
                 </ul>
             </div>
             <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <span className="hover:underline">Nemesis Team</span>. All Rights Reserved.
+            <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <span className="hover:underline">ASVARAN GROUP</span>. All Rights Reserved.
             </span>
         </footer>
     );
