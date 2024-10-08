97c3cde4084a3c320b3cf2d34d3bf0a22717859d
Armin Amiri <78607877+zNiGhTFuRyZNTT@users.noreply.github.com>
2024-10-08 21:37:38 +1100

    Self intro update

--- src/components/InfoCard/index.jsx
+++ src/components/InfoCard/index.jsx
@@ -11,88 +11,120 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import { faUniversity } from "@fortawesome/free-solid-svg-icons";
 
+import BinaryImg from '../../assets/images/binary.jpg'
+// import BinaryImg2 from '../../assets/images/binary1.jpg'
+
+// InfoCard component displays personal information and social links
+// It receives an 'inView' prop to control its visibility animation
 const InfoCard = ({ inView }) => {
 return (
         
-            <div className={`px-6 ${inView ? "transition" : "ziresh"}`}>
-                <div className="flex flex-wrap justify-center">
-                    <div className="w-full px-4 flex justify-center">
-                        <div className="picture-container profile-pic relative h-auto align-middle  -m-16 -ml-17 lg:-ml-16 max-w-150-px">
-                            <img alt="Grrrr" src={Armin}
-                                className="rounded-full border-none shadow-xl" />
-                        </div>
-                    </div>
-                </div>
-                <div className="text-center header-text-center">
-                    <h3 className="text-xl font-semibold leading-normal mb-2 text-blueGray-700">
-                        Armin Amiri
-                    </h3>
-                    <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
-                        <i className="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
-                        Melbourne, Australia
-                    </div>
-                    <div className="mb-2 text-blueGray-600 mt-10">
-                        <i className="fas fa-briefcase mr-2 text-lg text-blueGray-400">Background Information</i>
-                        {/* ["Javascript", "Python"].forEach(language =&gt; `$&#123;language&#125; Developer` ) */}
-                        <div className="codeblock">
-                            <CopyBlock
-                                language="js"
-                                text={'//Work Availability\n let available = true \n// Programming Languages\n ["Javascript", "Python"].forEach(language => `${language} Developer`)\n // Spoken Languages\n const spokenLangs = ["Persian", "English"]\n // Work and Eduaction\n let [educationStatus, workStatus] = ["Recent graduate", "Unemployed"]\n // Contact Details \n const contactDetails = {\nphoneNum: "+61403444370",\n email: "arminamiri11228@gmail.com"\n}\n // Contact me through phone or email for any inquiries or job oportunities'}
-                                showLineNumbers={true}
-                                theme={dracula}
-                                wrapLines={true}
-                                codeBlock
-                            />
-                        </div>
+    // Main container with conditional classes for visibility animation
+    <div className={`px-6 ${inView ? "transition" : "ziresh"} h-full`}        style={{
+        backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url(${BinaryImg})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}>
 
-                    </div>
-                    <div className="flex flex-col mt-6 items-center mb-2 w-full text-blueGray-600">
-                        <div className="flex items-center">
-                        <FontAwesomeIcon icon={faUniversity} className="mr-2 text-lg text-blueGray-400" />
-                        <h1>Studying at <span className="font-medium whitespace-nowrap">Swinburne University</span></h1>
-                        </div>
-                    </div>
-                </div>
-                <div className=" mt-10 py-10 border-t border-blueGray-200 text-center">
-                    <div className="mb-5 flex flex-wrap justify-center text-center ">
-                        <div className=" bg-info-txt-container w-full lg:w-9/12 px-4">
-                            {/* <styledWord word={"Armin"} /> */}
-                            <p className="bg-info-description text-left ml-9 text-md leading-relaxed text-blueGray-700">
+        {/* Profile picture section */}
 
-                                <span className="flex flex-row bg-info-nametitle mb-4 text-xl w-full font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">
-                                    <span className="underline underline-offset-3 decoration-8 decoration-blue-600">
-                                        I'm Armin
-                                    </span>
-                                </span>
+        
+        {/* Personal information section */}
+        <div className="flex flex-col xl:flex-row xl:gap-20 justify-evenly items-center text-center h-full">    
 
-                                
-                                a dedicated software developer with expertise in Python, JavaScript, and Node. My passion for programming ignited during my early teenage years and has only grown stronger since then. Guided by my thirst for knowledge and problem-solving skills, I immersed myself in Python for two years, honing my skills with tireless determination.
-
-                                As I continued to learn and explore, I discovered the beauty of JavaScript, captivated by its unique syntax and industry relevance. My interest in Node led me to learn JavaScript directly, and I quickly gained proficiency in the language. Although I initially thought I disliked web development, my outlook changed after I discovered React, which shifted my path towards a career in web development.
-
-                                I'm based in Melbourne and have a strong passion for coding. My coding record speaks for itself, with a staggering 13-hour stretch of uninterrupted work and only brief 10-minute breaks. My expertise and dedication to programming make me a valuable asset to any organization, and I'm constantly seeking new challenges and opportunities to grow.
-
-                                If you're searching for a talented software developer with a strong work ethic and a passion for programming, look no further. I'm currently seeking new opportunities to further develop my skills, and I welcome any job opportunities that align with my abilities. If you're interested in collaborating, don't hesitate to contact me.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="makeCenter w-full icons flex justify-center ">
-                        <a className="icon fa-2x" href="https://github.com/zNiGhTFuRyZNTT">
-                            <FontAwesomeIcon icon={faGithub} color="#171515" />
-                        </a>
-                        <a className="icon fa-2x" href="https://instagram.com/arminnightfury">
-                            <FontAwesomeIcon icon={faInstagram} color="#171515" />
-                        </a>
-                        <a className="icon fa-2x" href="https://www.linkedin.com/in/armin-amiri-4604781bb/">
-                            <FontAwesomeIcon icon={faLinkedin} color="#171515" />
-                        </a>
+            <div className="flex flex-row items-center mt-5 justify-between gap-3 sm:gap-10 md:gap-10 xl:gap-20">
+            <div className="flex flex-wrap justify-center w-48 sm:w-72 lg:w-96">
+                <div className="w-full px-2 flex justify-center">
+                    <div className="picture-container profile-pic relative h-auto align-middle">
+                        <img alt="Avatar" src={Armin} className="rounded-full border-none shadow-xl w-full" />
                     </div>
                 </div>
             </div>
+            <div className="flex flex-col items-center justify-around gap-2 md:gap-9 h-full">
+                <h3 className=" text-[1.6rem] sm:text-5xl  md:text-5xl lg:text-6xl font-semibold leading-normal mb-2 text-blueGray-700">
+                    Armin Amiri
+                </h3>
+
+                <div className="md:grid md:grid-cols-2 md:gap-4 ">
+
+                    <h3 className="text-[.7rem] sm:text-xl md:text-xl font-medium sm:font-normal leading-normal mb-2 text-blueGray-700">
+                        🧑🏻‍💻 Full Stack Developer
+                    </h3>
+                    <h3 className="text-[.7rem] sm:text-xl md:text-xl font-medium sm:font-normal leading-normal mb-2 text-blueGray-700">
+                        <span className="sm:text-xl md:text-xl">📍</span> Melbourne, Australia
+                    </h3>
+
+                </div>
+                    {/* Education information */}
+                    <div className="flex flex-col mt-3 sm:mt-6 items-center mb-2 w-full text-blueGray-600">
+                        <div className="flex items-center">
+                            <FontAwesomeIcon icon={faUniversity} className="mr-2 text-lg text-blueGray-400" />
+                            <h1 className="text-xs sm:text-lg">Studying at <span className="font-medium whitespace-nowrap">Swinburne University</span></h1>
+                        </div>
+                    </div>
+
+            </div>
+            </div>
+            
+            {/* Code block section displaying background information */}
+            <div className="mb-2 text-blueGray-600 mt-10">
+                {/* <i className="fas fa-briefcase mr-2 text-lg text-blueGray-400">Background Information</i> */}
+
+                <div className="codeblock">
+                    <CopyBlock
+                        language="js"
+                        text={'//Work Availability\n let available = true \n// Programming Languages\n ["Javascript", "Python"].forEach(language => `${language} Developer`)\n // Spoken Languages\n const spokenLangs = ["Persian", "English"]\n // Work and Eduaction\n let [educationStatus, workStatus] = ["Recent graduate", "Unemployed"]\n // Contact Details \n const contactDetails = {\nphoneNum: "+61403444370",\n email: "arminamiri11228@gmail.com"\n}\n // Contact me through phone or email for any inquiries or job oportunities'}
+                        showLineNumbers={true}
+                        theme={dracula}
+                        wrapLines={true}
+                        codeBlock
+                    />
+                </div>
+
+            </div>
+            
+        </div>
+        
+        {/* Detailed personal description */}
+        <div className=" mt-10 py-10 border-t border-blueGray-200 text-center">
+            <div className="mb-5 flex flex-wrap justify-center text-center ">
+                <div className=" bg-info-txt-container w-full lg:w-9/12 px-4">
+                    <p className="bg-info-description text-left ml-9 text-md leading-relaxed text-blueGray-700">
+
+                        <span className="flex flex-row bg-info-nametitle mb-4 text-xl w-full font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">
+                            <span className="underline underline-offset-3 decoration-8 decoration-blue-600">
+                                I'm MR. Robot
+                            </span>
+                        </span>
+
+                        A dedicated software developer with expertise in Python, JavaScript, and Node. My passion for programming ignited during my early teenage years and has only grown stronger since then. Guided by my thirst for knowledge and problem-solving skills, I immersed myself in Python for two years, honing my skills with tireless determination.
+
+                        As I continued to learn and explore, I discovered the beauty of JavaScript, captivated by its unique syntax and industry relevance. My interest in Node led me to learn JavaScript directly, and I quickly gained proficiency in the language. Although I initially thought I disliked web development, my outlook changed after I discovered React, which shifted my path towards a career in web development.
+
+                        I'm based in Melbourne and have a strong passion for coding. My coding record speaks for itself, with a staggering 13-hour stretch of uninterrupted work and only brief 10-minute breaks. My expertise and dedication to programming make me a valuable asset to any organization, and I'm constantly seeking new challenges and opportunities to grow.
+
+                        If you're searching for a talented software developer with a strong work ethic and a passion for programming, look no further. I'm currently seeking new opportunities to further develop my skills, and I welcome any job opportunities that align with my abilities. If you're interested in collaborating, don't hesitate to contact me.
+                    </p>
+                </div>
+            </div>
+            
+            {/* Social media links */}
+            <div className="makeCenter w-full icons flex justify-center ">
+                <a className="icon fa-2x" href="https://github.com/zNiGhTFuRyZNTT">
+                    <FontAwesomeIcon icon={faGithub} color="#171515" />
+                </a>
+                <a className="icon fa-2x" href="https://instagram.com/arminnightfury">
+                    <FontAwesomeIcon icon={faInstagram} color="#171515" />
+                </a>
+                <a className="icon fa-2x" href="https://www.linkedin.com/in/armin-amiri-4604781bb/">
+                    <FontAwesomeIcon icon={faLinkedin} color="#171515" />
+                </a>
+            </div>
+        </div>
+    </div>
 
 )
 }
-
-
 
 export default InfoCard;
--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -30,33 +30,56 @@ const LandingPage = () => {
     return () => clearTimeout(timeout)
   }, []);
 
-    const greetings = ['Hello friend,','Welcome to my portfolio.']
-    const greetingsRef = useRef()
-    const greetingInView = useIntersection(greetingsRef)
+    // each object inside this array will be shown with an animation
+    // upon loading of the landing page.
+    // G0
+    const greetings = [
+      { text: 'Hello friend,', delay: 1000 },
+      { text: 'Welcome to my portfolio.', delay: 3000 },
+      { text: 'Scroll down', delay: 6000 }
+    ] 
 
-    const certificatesRef = useRef()
-    const certificatesInView = useIntersection(certificatesRef, '-100px')
-    
-    const infoCardRef = useRef()
-    const infoCardInView = useIntersection(infoCardRef, '-100px')
+    // refrences to attach to different components of the page
+    // in order to track whether they are inside the device viewport
+    const refs = {
+      greetings: useRef(),
+      certificates: useRef(),
+      infoCard: useRef(),
+      projectCard: useRef(),
+      skills: useRef(),
+    };
+  
+    // useIntersection hook returns a boolean (true, false)
+    // and indicates whether the component we attached it to is visible in the viewport.
+    // meaning when the client scrolls through the website and reaches the target component
+    // we then use this boolean values to unhide the components with a pleasing animation
+    // when the client scrolls on them.
 
-    const projectCardRef = useRef()
-    const projectCardInView = useIntersection(projectCardRef, '-100px')
-
-    const skillsRef = useRef()
-    const skillsInView = useIntersection(skillsRef, '-70px')
-
+    // '-100px' tells the hook to return true as soon as the user reached -100 pixels of the component.
+    // meaning the component will appear when you are 100 pixels away from scrolling on it.
     const inViews = {
-      certificates: certificatesInView,
-      infoCard: infoCardInView,
-      projectCard: projectCardInView,
-      skills: skillsInView,
-    }
+      greetings: useIntersection(refs.greetings),
+      certificates: useIntersection(refs.certificates, '-100px'),
+      infoCard: useIntersection(refs.infoCard, '-100px'),
+      projectCard: useIntersection(refs.projectCard, '-100px'),
+      skills: useIntersection(refs.skills, '-70px'),
+    };
     
     // < -- Handle Window Size -- >
 
     const [windowSize, setWindowSize] = useState(getWindowSize());
 
+    // You can remove this, this is used for the developer
+    // device size doesn't usually change unless you change it from inspect element.
+    // this ensures someof our components that their width is calculated based on the windowsize
+    // remain in place and don't mess the page up on window size change inside inspect element.
+
+    //  IT IS RECOMMENDED TO LEAVE IT AS IT IS.
+    // because either as the developer you don't want some elements mess up the page
+    // when you are testing something like responsiveness inside inspect elenent,
+    // or as the user who wants to test the responsibility of your website.
+    // the smooth transition applied to the components will show how precise these calcculations are
+    // done and will leave the client speechless on how the components resize on window size changes
     useEffect(() => {
       function handleWindowResize() {
         setWindowSize(getWindowSize());
@@ -70,9 +93,6 @@ const LandingPage = () => {
     }, []);
     // < -- End -- >
 
-    // const summaryInView = useIntersection(summaryRef, '-5px')
-    // console.log(greetingInView, infoCardInView);
-    // console.log(inViews);
 
     return (
       <>
@@ -80,66 +100,84 @@ const LandingPage = () => {
         <ClipLoader color="#1c64f2" />
       </div>
       <div className="landingPage"> 
-          <div className={!greetingInView ? "transition-all ease-out duration-300 opacity-100": "opacity-0 transition-all ease-in duration-450 translate-x-100"}>
-            {!(windowSize.innerWidth < 535) && <SideBar inViews={inViews} />} {/* TODO Slide from left to right animation */}
-          </div>
-          <div className={`main-content ${!greetingInView ? "transition-all ease-out duration-300" : ""} `}>
-            <SquareBackground />
-            <div ref={greetingsRef} className="greetings">
-              <VideoBackground />
-              <div className={"center"}>
-                <AnimatedLetters delay={1000} strArray={greetings[0].split("")} idx={3} />
-                <AnimatedLetters delay={3000} strArray={greetings[1].split("")} idx={3} />
-                <AnimatedLetters delay={6000} strArray={"Scroll down".split("")} idx={3} />
-                {/* <div className="sidebar-icon">
-                  <FaArrowDown size="20" />
 
-                </div> */}
+          {/* Showing the side menu if the device is not a phone or a small tablet */}
+          <div className={!inViews.greetings ? "transition-all ease-out duration-300 opacity-100": "opacity-0 transition-all ease-in duration-450 translate-x-100"}>
+            {!(windowSize.innerWidth < 535) && <SideBar inViews={inViews} />} 
+          </div>
+
+          <div className={`main-content ${!inViews.greetings ? "transition-all ease-out duration-300" : ""} `}>
+
+            {/* 
+            SquareBackground
+              Little random animated squares ascending from the bottom of the screen,
+              Giving the website a live and interactive feeling.
+            
+            */}
+            <SquareBackground />
+
+            <div ref={refs.greetings} className="greetings">
+              {/*
+              VideoBackground 
+                Display the video as the background 
+              */}
+              <VideoBackground />
+
+              <div className={"center"}>
+                {/*
+                 Loooping over the texts to be shown on the landing page
+                 to go to the array search G0 in this code.
+                 */}
+                {greetings.map((item, index) => (
+                  <AnimatedLetters delay={item.delay} strArray={item.text.split("")} idx={3} key={index} />
+                ))}
               </div>
             </div>
               
 
-            {/* < -- Info Card -- >  */}
+            {/* < -- Your Introduction Component -- >  */}
             <div className="bg-white info-cards" > 
-              <div ref={infoCardRef}>
-                <InfoCard inView={infoCardInView}/>
+              <div ref={refs.infoCard}>
+                <InfoCard inView={inViews.infoCard}/>
               </div>
             </div>
-            {/* < -- end -- >  */}
 
-              <div ref={skillsRef} className="skills-root-container">
-                <h1 className="cert-header mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-white">
-                  <span className="underline underline-offset-3 decoration-8 decoration-blue-600">
-                    Skills
-                  </span>
-                </h1>
-                <SkillsRating inView={skillsInView} windowSize={windowSize}/>
-              </div>
+            {/* < -- Your Sills Component -- >  */}
+            <div ref={refs.skills} className="skills-root-container">
+              <h1 className="cert-header mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-white">
+                <span className="underline underline-offset-3 decoration-8 decoration-blue-600">
+                  Skills
+                </span>
+              </h1>
+              <SkillsRating inView={inViews.skills} windowSize={windowSize}/>
+            </div>
 
-            {/* < -- Certificates -- >  */}
-            <div ref={certificatesRef} className="certifications-container-root">
+
+            {/* < -- Certificates Component -- >  */}
+            <div ref={refs.certificates} className="certifications-container-root">
               <h1 className="cert-header mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-white"><span className="underline underline-offset-3 decoration-8 decoration-blue-600">Certificates</span></h1>
-              <Certifications inView={certificatesInView} />
+              <Certifications inView={inViews.certificates} />
             </div>
-            {/* < -- end -- >  */}
 
-            {/* < -- My comment -- >  */}
+
+            {/* < -- Background Info Component -- >  */}
             <div className="freelance-inf w-screen bg-white  ">
               <BackgroundInfo />
             </div>
-            {/* < -- end -- >  */}
+
 
             <hr />
             
-            {/* < -- Project -- >  */}
-            <div ref={projectCardRef} className="projects-container mt-5 dark">
+            {/* < -- Projects Component -- >  */}
+            <div ref={refs.projectCard} className="projects-container mt-5 dark">
               <h1 className="cert-header mb-10 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-white"><span className="underline underline-offset-3 decoration-8 decoration-blue-600">Projects</span></h1>
-              <Projects inView={projectCardInView} />
+              <Projects inView={inViews.projectCard} />
             </div>
             {/* < -- end -- >  */}
 
           </div>
 
+          {/* Display Footer */}
           <Footer />
         </div>
       </>
